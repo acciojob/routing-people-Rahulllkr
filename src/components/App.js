f34ec45946f69da995f278029ef50682bb531220
Rahulllkr <rahulkumar8604745@gmail.com>
2025-10-31 16:21:44 +0530

    check now

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,40 @@
 
 import React from "react";
 import './../styles/App.css';
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import users from "./users";
+
+
+
 
 const App = () => {
+const names = [
+  "Leanne Graham",
+  "Ervin Howell",
+  "Clementine Bauch",
+  "Patricia Lebsack",
+  "Chelsey Dietrich",
+  "Mrs. Dennis Schulist",
+  "Clementina DuBuque"
+];
   return (
+    <BrowserRouter>
     <div>
         {/* Do not remove the main div */}
-        <h1>Hello Routes</h1>
+        <h1>User List</h1>
+        <div>
+          {
+          names.map((uName,index) => {
+            return <div key={index}><Link to={`/users/:${index+1}`}><p>{uName}</p></Link></div>
+          })
+        }
+        <Routes>
+          <Route path="/users/:num" element={<Users />}/>
+        </Routes>
+        </div>
+
     </div>
+    </BrowserRouter>
   )
 }
 
